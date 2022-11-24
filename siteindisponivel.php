cec7d5bc5b8aeea45f0621eee63f0688bae23915
Bleidson Pereira Nunes <106042382+Bleidson@users.noreply.github.com>
2022-11-24 21:40:04 -0200

    Initial commit

---
+++ siteindisponivel.php
@@ -0,0 +1,3 @@
+        !!!Impossivel acessar essa guia!!!
+Este site está em desenvolvimento, e esta é só uma amostra.
+Este site é só uma maneira que encontrei de praticar e por meus conhcimento em jogo.
